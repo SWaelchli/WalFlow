72d9e3846897597710e315803404cf6599d096df
Sebastian <sebastian.waelchli@gmail.com>
2026-08-04 13:21:14 -0500

    chore: release v0.1.4

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 // Centralized WalFlow Application Constants & Version Control
 
 export const APP_VERSION = '0.1.4';
-export const RELEASE_STAGE = 'Beta'; // Options: 'Alpha' | 'Beta' | 'Pre-Release' | 'Release'
+export const RELEASE_STAGE = 'Release'; // Options: 'Alpha' | 'Beta' | 'Pre-Release' | 'Release'
 export const FILE_FORMAT_VERSION = '0.1';
 export const FILE_EXTENSION = '.wlf';
 
